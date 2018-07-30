7ac027ea2bee7ace76f666a34e95f91856342e65
sangviehuy <hvsang2101@gmail.com>
2018-07-30 19:53:12 +0700

    UpdateMyPortfolio

--- js/mainjs.js
+++ js/mainjs.js
@@ -17,17 +17,17 @@ $('.list-services-slide').owlCarousel({
     nav:false,
     dots:true,
     autoplay: true,
-    autoplayTimeout:1000,
+    autoplayTimeout:10000,
     autoplayHoverPause:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:1
         },
         1000:{
-            items:3
+            items:1
         }
     }
 })
